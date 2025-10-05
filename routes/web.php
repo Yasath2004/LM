cc819ee6ae28d92bec0b374c5e7b319f484aa619
Yasath2004 <yasathamasara2@gmail.com>
2025-10-05 17:41:23 +0530

    creat a new data base

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [Templatecontroller::class, 'index']);
 
 Route::get('/loans', [Templatecontroller::class, 'loans']);
+
+Route::get('/payments', [Templatecontroller::class, 'payments']);
+
+Route::get('/settings', [Templatecontroller::class, 'settings']);
+
+Route::get('/collections', [Templatecontroller::class, 'collection']);
